803461e027e5bf33f7eb27c3c8ba85842057c4cf
Cuongvd <vuduccuong.ck48@gmail.com>
2020-02-18 11:08:23 +0700

    Ep.3:  Authen With Jwt

---
+++ Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Constants
+    {
+        public const string Issuer = Audiance;
+        public const string Audiance = "https://localhost:44358/";
+        public const string Secret = "Bong_Ai_Do_Nhe_Nhang_Vut_Qua_Noi_Day";
+    }
+}
---
+++ Server/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authentication()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "cvd_id"),
+                new Claim("granmy", "cookie")
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCreadentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audiance,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCreadentials
+                );
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new { access_token = tokenJson });
+        }
+    }
+}
